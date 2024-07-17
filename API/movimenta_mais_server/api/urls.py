a8dec8634650e3366899c8423f3ecacc49d10c40
Your Name <you@example.com>
2024-07-17 10:28:57 -0300

    Ajustes

--- API/movimenta_mais_server/api/urls.py
+++ API/movimenta_mais_server/api/urls.py
@@ -4,29 +4,29 @@ from django.conf.urls.static import static
 
 from .auth import views_auth
 from .Idoso_Atividades import views_atendimento, views_idoso, views_treino,views_avaliacao
-from .Docs import views_exams
+from .Forms import views_exams
 
 urlpatterns = [
     path('user_admin/', views_auth.User_AdminListCreate.as_view(), name="user_admin_list_create"),
     # Paths de Auth
     path('user_admin/<int:pk>/', views_auth.User_AdminRetrieveUpdateDestroy.as_view(), name="user_admin_retrieve_update_destroy"),
-    path('user_admin/list/', views_auth.User_AdminList.as_view(), name='user_admin_list'),
+    path('user_admin/lista/', views_auth.User_AdminList.as_view(), name='user_admin_list'),
     path('signin/', views_auth.sign_in, name='sign_in'),
     # Paths de Idoso_Atividades
     path('idosos_dados/cadastrar/', views_idoso.Idoso_DadosListCreate.as_view(), name="idoso_dados_cadastrar"),
     path('idosos_dados/<int:pk>/', views_idoso.Idoso_DadosRetrieveUpdateDestroy.as_view(), name="idoso_dados_retrieve_update_destroy"),
-    path('idosos_dados/list/', views_idoso.Idoso_DadosList.as_view(), name='idosos_dados_list'),
+    path('idosos_dados/lista/', views_idoso.Idoso_DadosList.as_view(), name='idosos_dados_list'),
     # Parte de Atendimento
     path('idosos_dados/atendimento/criar/', views_atendimento.AtendimentoListCreate.as_view(), name="idoso_atendimento_criar"),
     path('idosos_dados/atendimento/lista/', views_atendimento.AtendimentoListByCPF.as_view(), name="idoso_atendimento_lista"),
     # Treino
-    path('idosos_dados/treino/create/', views_treino.TreinoCreateAPIView.as_view(), name='treino_create'),
+    path('idosos_dados/treino/criar/', views_treino.TreinoCreateAPIView.as_view(), name='treino_create'),
     path('idosos_dados/treino/<int:pk>/', views_treino.TreinoRetrieverUpdateDestory.as_view(), name='treino_retrieve_update_destroy'),
     path('idosos_dados/treino/listar/', views_treino.TreinoList.as_view(), name='treino_listar'),
-    path('idosos_dados/avaliacao/create/', views_avaliacao.AvaliacaoCreateAPIView.as_view(), name='avaliacao_criar'),
+    # Avaliação
+    path('idosos_dados/avaliacao/criar/', views_avaliacao.AvaliacaoCreateAPIView.as_view(), name='avaliacao_criar'),
     path('idosos_dados/avaliacao/listar/', views_avaliacao.AvaliacaoList.as_view(), name='avaliacao_listar'),
-    path('idosos_dados/avaliacao/<int:pk>', views_avaliacao.AvaliacaoRetrieverUpdateDestory.as_view(), name='avaliacao_deletar_editar'),
-    
+    path('idosos_dados/avaliacao/<int:pk>', views_avaliacao.AvaliacaoRetrieverUpdateDestory.as_view(), name='avaliacao_deletar_editar'),  
     # Paths de Docs
     path('docs/exame/upload', views_exams.upload_exame_view, name='upload_exame'),
     path('docs/exame/lista', views_exams.ExameListByCPF.as_view(), name='upload_lista'),
