b07693966db3369f778bd880901c211ef8f1f356
Ana Karla <anakarlad.21@gmail.com>
2024-07-16 21:58:43 -0300

    rota para listar todos os treinos

--- API/movimenta_mais_server/api/Idoso_Atividades/views_treino.py
+++ API/movimenta_mais_server/api/Idoso_Atividades/views_treino.py
@@ -19,11 +19,31 @@ class TreinoList(APIView):
               serializer = TreinoSerializer(treinos, many=True)
               return Response(serializer.data, status=status.HTTP_200_OK)
           else:
-              return Response({"detail": "Treinos não encontrados."}, status=status.HTTP_404_NOT_FOUND)
+              return Response({"detail": "Treinos não encontrados para o cpf fornecido."}, status=status.HTTP_404_NOT_FOUND) #retorna os treinos do cpf 
       else:
-          return Response({"detail": "CPF não fornecido."}, status=status.HTTP_400_BAD_REQUEST)
-   
+        treinos = Treino.objects.all() 
+        serializer = TreinoSerializer(treinos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK) #retorna todos os treinos 
+      
 class TreinoRetrieverUpdateDestory(generics.RetrieveUpdateDestroyAPIView):
   queryset = Treino.objects.all()
   serializer_class = TreinoSerializer
   lookup_field = 'pk' 
+
+  def put(self, request, *args, **kwargs):
+    try:
+      instance = self.get_object()
+      serializer = TreinoSerializer(instance, data=request.data)
+      if serializer.is_valid():
+          serializer.save()
+          return Response(serializer.data, status=status.HTTP_200_OK)
+    except:    
+      return Response({"detail": "Requisição inválida."}, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, *args, **kwargs):
+    try:
+      instance = self.get_object() 
+      instance.delete()
+      return Response({"detail": "Treino deletado com sucesso."}, status=status.HTTP_204_NO_CONTENT)    
+    except:
+      return Response({"detail": "Requisição inválida."}, status=status.HTTP_400_BAD_REQUEST)
